0b2e4e6fb5a632072d167f5ba6ad2fb97cccb4e0
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-02 12:22:37 +0530

    "Added a mediaController view to the app with the functionality that when play button is clicked the music provided starts playing and pauses on clicking of the pause button, but there seems to be a bug right now causing music to not play. Will be fixed in future commits."

--- app/src/main/java/com/example/audiovisual/MainActivity.java
+++ app/src/main/java/com/example/audiovisual/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.audiovisual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -14,25 +16,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     VideoView videoView;
     Button button;
     MediaController mediaController;
+    Button playMusic, pauseMusic;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoView = findViewById(R.id.videoView);
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.playVideo);
         button.setOnClickListener(MainActivity.this);
+        playMusic = findViewById(R.id.playMusic);
+        pauseMusic = findViewById(R.id.pauseMusic);
         mediaController = new MediaController(MainActivity.this);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
     }
 
     @Override
-    public void onClick(View v) {
-        Uri videoUri = Uri.parse("android.resource://"+ getPackageName() + "/"
+    public void onClick(View buttonView) {
+
+
+        switch (buttonView.getId()) {
+
+            case R.id.playMusic : {
+                mediaPlayer.start();
+                Log.i("myTag","Play Music Button is entered");
+                break; }
+
+
+
+            case R.id.playVideo : {
+                Uri videoUri = Uri.parse("android.resource://"+ getPackageName() + "/"
                         + R.raw.video);
-        videoView.setVideoURI(videoUri);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
-        videoView.start();
+                videoView.setVideoURI(videoUri);
+                videoView.setMediaController(mediaController);
+                mediaController.setAnchorView(videoView);
+                videoView.start();
+                Log.i("video","Video case entered");
+                break;
+            }
+
+
+
+
+            case R.id.pauseMusic : {
+                mediaPlayer.stop();
+                break;
+            }
+        }
 
 
     }
