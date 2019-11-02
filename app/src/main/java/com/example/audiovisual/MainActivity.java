48683adad82486bf88ea1890c29b6c74760636f2
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-02 12:54:26 +0530

    "Added a seekBar to change the volume of the music within the App but the previous bug of music not playing is yet to be solved, due to this feature hasn't been put to the test yet."

--- app/src/main/java/com/example/audiovisual/MainActivity.java
+++ app/src/main/java/com/example/audiovisual/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.audiovisual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MediaController mediaController;
     Button playMusic, pauseMusic;
     MediaPlayer mediaPlayer;
+    SeekBar seekBar;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pauseMusic = findViewById(R.id.pauseMusic);
         mediaController = new MediaController(MainActivity.this);
         mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        seekBar = findViewById(R.id.seekBar);
+        //Since audioManager wants a systemService ,we pass it and typecast it to AudioManager
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        //Each user's device may have a different maximum volume, so we need to access it
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        //Lets also get the current volume of the user's device
+
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //It makes sense to set the maximum value of the volume seekbar as the maxVolume
+
+        seekBar.setMax(maxVolume);
+
+        //Also we have to set the current volume as the current progress of the seekbar
+
+        seekBar.setProgress(currentVolume);
+
+
+        // We have something called onSeekBar method which will deal with the changes in seekbar
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            // This is an anonymous Inner Class that is implementing the OnSeekBarChangeListener method
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                if (fromUser) {
+                    Toast.makeText(MainActivity.this, Integer.toString(progress), Toast.LENGTH_SHORT).show();
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
